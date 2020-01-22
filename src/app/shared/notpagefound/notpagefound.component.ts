846d7dd45f60ed9e27fcbea84138df2b3a5cd172
yfigueroa <yfsanchez@gmail.com>
2020-01-22 12:08:31 -0300

    first commit

---
+++ src/app/shared/notpagefound/notpagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+
+})
+export class NotpagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
